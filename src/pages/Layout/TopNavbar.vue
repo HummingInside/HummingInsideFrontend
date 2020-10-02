b689363d4919ad5ae8b915e6dfb6946a73cbd0cf
myungsegyo <myungsekyo@gmail.com>
2020-10-03 00:26:10 +0900

    [navbar] change top bar elements

--- src/pages/Layout/TopNavbar.vue
+++ src/pages/Layout/TopNavbar.vue
@@ -38,22 +38,22 @@
                  :show-close="true">
             <input slot="header" v-model="searchQuery" type="text" class="form-control" id="inlineFormInputGroup" placeholder="SEARCH">
           </modal>
-          <drop-down>
-            <a href="javascript:void(0)" data-toggle="dropdown" class="dropdown-toggle nav-link">
-              <div class="notification d-none d-lg-block d-xl-block"></div>
-              <i class="tim-icons icon-sound-wave"></i>
-              <p class="d-lg-none text-left">
-                Notifications
-              </p>
-            </a>
-            <ul class="dropdown-menu dropdown-menu-right dropdown-navbar">
-              <li class="nav-link"><a href="#" class="nav-item dropdown-item">Mike John responded to your email</a></li>
-              <li class="nav-link"><a href="javascript:void(0)" class="nav-item dropdown-item">You have 5 more tasks</a></li>
-              <li class="nav-link"><a href="javascript:void(0)" class="nav-item dropdown-item">Your friend Michael is in town</a></li>
-              <li class="nav-link"><a href="javascript:void(0)" class="nav-item dropdown-item">Another notification</a></li>
-              <li class="nav-link"><a href="javascript:void(0)" class="nav-item dropdown-item">Another one</a></li>
-            </ul>
-          </drop-down>
+<!--          <drop-down>-->
+<!--            <a href="javascript:void(0)" data-toggle="dropdown" class="dropdown-toggle nav-link">-->
+<!--              <div class="notification d-none d-lg-block d-xl-block"></div>-->
+<!--              <i class="tim-icons icon-sound-wave"></i>-->
+<!--              <p class="d-lg-none text-left">-->
+<!--                Notifications-->
+<!--              </p>-->
+<!--            </a>-->
+<!--            <ul class="dropdown-menu dropdown-menu-right dropdown-navbar">-->
+<!--              <li class="nav-link"><a href="#" class="nav-item dropdown-item">Mike John responded to your email</a></li>-->
+<!--              <li class="nav-link"><a href="javascript:void(0)" class="nav-item dropdown-item">You have 5 more tasks</a></li>-->
+<!--              <li class="nav-link"><a href="javascript:void(0)" class="nav-item dropdown-item">Your friend Michael is in town</a></li>-->
+<!--              <li class="nav-link"><a href="javascript:void(0)" class="nav-item dropdown-item">Another notification</a></li>-->
+<!--              <li class="nav-link"><a href="javascript:void(0)" class="nav-item dropdown-item">Another one</a></li>-->
+<!--            </ul>-->
+<!--          </drop-down>-->
           <drop-down>
                 <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
                   <div class="photo">
