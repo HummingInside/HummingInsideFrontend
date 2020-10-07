fded35c5951bc4cbd22b1ecbf7977e3fe12ee25b
myungsegyo <myungsekyo@gmail.com>
2020-10-07 15:00:47 +0900

    [auth] add sign-in, sign-up components

---
+++ src/pages/SignIn.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        로그인
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SignUp"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -8,85 +8,98 @@ import UserProfile from "@/pages/UserProfile.vue";
 import TableList from "@/pages/TableList.vue";
 import Typography from "@/pages/Typography.vue";
 import ConcertDetail from "./pages/ConcertDetail";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 
-const routes = [{
-  path: "/",
-  component: BaseLayout,
-  redirect: "dashboard",
-  children:[
-    {
-      path: "concerts/:pk",
-      name: "concert-detail",
-      component: ConcertDetail,
-    },
-    {
-      path: "concerts",
-      name: "concert-list",
-      component: ConcertList,
-    },
-    {
-      path: "dashboard",
-      name: "New & Hot",
-      component: ConcertList
-    },
-    {
-      path: "a",
-      name: "New & Hot",
-      component: ConcertList
-    },
-    {
-      path: "b",
-      name: "Ongoing",
-      component: ConcertList
-    },
-    {
-      path: "c",
-      name: "Upcoming",
-      component: ConcertList
-    },
-    {
-      path: "d",
-      name: "My Concerts",
-      component: ConcertList
-    },
-    {
-      path: "e",
-      name: "My Tickets",
-      component: ConcertList
-    },
-    {
-      path: "icons",
-      name: "Icons",
-      component: Icons
-    },
-    {
-      path: "maps",
-      name: "Maps",
-      component: Maps
-    },
-    {
-      path: "notifications",
-      name: "Notifications",
-      component: Notifications
-    },
-    {
-      path: "user",
-      name: "User Profile",
-      component: UserProfile
-    },
-    {
-      path: "table",
-      name: "Table List",
-      component: TableList
-    },
-    {
-      path: "typography",
-      name: "Typography",
-      component: Typography
-    }
-  ]
+const routes = [
+  {
+    path: "/sign-in",
+    name: 'sign-in',
+    component: SignIn,
+  },
+  {
+    path: "/sign-up",
+    name: 'sign-up',
+    component: SignUp,
+  }
+  ,{
+    path: "/",
+    component: BaseLayout,
+    redirect: "dashboard",
+    children:[
+      {
+        path: "concerts/:pk",
+        name: "concert-detail",
+        component: ConcertDetail,
+      },
+      {
+        path: "concerts",
+        name: "concert-list",
+        component: ConcertList,
+      },
+      {
+        path: "dashboard",
+        name: "New & Hot",
+        component: ConcertList
+      },
+      {
+        path: "a",
+        name: "New & Hot",
+        component: ConcertList
+      },
+      {
+        path: "b",
+        name: "Ongoing",
+        component: ConcertList
+      },
+      {
+        path: "c",
+        name: "Upcoming",
+        component: ConcertList
+      },
+      {
+        path: "d",
+        name: "My Concerts",
+        component: ConcertList
+      },
+      {
+        path: "e",
+        name: "My Tickets",
+        component: ConcertList
+      },
+      {
+        path: "icons",
+        name: "Icons",
+        component: Icons
+      },
+      {
+        path: "maps",
+        name: "Maps",
+        component: Maps
+      },
+      {
+        path: "notifications",
+        name: "Notifications",
+        component: Notifications
+      },
+      {
+        path: "user",
+        name: "User Profile",
+        component: UserProfile
+      },
+      {
+        path: "table",
+        name: "Table List",
+        component: TableList
+      },
+      {
+        path: "typography",
+        name: "Typography",
+        component: Typography
+      }
+    ]
 
-}];
+  }];
 
 export default routes;
