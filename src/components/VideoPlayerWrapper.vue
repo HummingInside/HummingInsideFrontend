aa4c7c8149b7e80362c0ec70ee6f46126e231243
myungsegyo <myungsekyo@gmail.com>
2020-10-30 22:14:23 +0900

    [video-player] add video player wrapper component

---
+++ src/components/VideoPlayerWrapper.vue
@@ -0,0 +1,39 @@
+<template>
+    <VideoPlayer class="vjs-custom-skin" :options="playerOptions">
+    </VideoPlayer>
+</template>
+
+<script>
+    import {videoPlayer} from "../vue-video-player"
+    import "../vue-video-player/custom-theme.css"
+
+    export default {
+        name: "VideoPlayerWrapper",
+        components: {
+            VideoPlayer: videoPlayer,
+        },
+        data() {
+            return {
+                playerOptions: {
+                    height: '706px',
+                    autoplay: true,
+                    muted: true,
+                    language: 'en',
+                    playbackRates: [0.7, 1.0, 1.5, 2.0],
+                    sources: [{
+                        type: "video/mp4",
+                        // mp4
+                        src: "http://vjs.zencdn.net/v/oceans.mp4",
+                        // webm
+                        // src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm"
+                    }],
+                    poster: "https://surmon-china.github.io/vue-quill-editor/static/images/surmon-1.jpg",
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
