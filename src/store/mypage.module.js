f9bb033b6360d11e9a33c4d53b563d69e27e2089
SimHongSub <ghdtjq2038@gmail.com>
2020-11-10 16:54:47 +0900

    [mypage] add module source for mypage graph

--- src/store/mypage.module.js
+++ src/store/mypage.module.js
@@ -15,14 +15,80 @@ export const state = {
     errors: null,
     concerts: [],
     reservations: [],
+    totalRevenue: [],
+    concertRevenue: [],
     expStatistics: {},
     isLoading: true,
+    bigLineChart: {
+        chartData: {
+            labels: [],
+            datasets: [{
+                fill: true,
+                borderColor: config.colors.primary,
+                borderWidth: 2,
+                borderDash: [],
+                borderDashOffset: 0.0,
+                pointBackgroundColor: config.colors.primary,
+                pointBorderColor: 'rgba(255,255,255,0)',
+                pointHoverBackgroundColor: config.colors.primary,
+                pointBorderWidth: 20,
+                pointHoverRadius: 4,
+                pointHoverBorderWidth: 15,
+                pointRadius: 4,
+                data: []
+            }]
+        },
+        extraOptions: chartConfigs.purpleChartOptions,
+        gradientColors: config.colors.primaryGradient,
+        gradientStops: [1, 0.4, 0]
+    },
+    greenLineChart: {
+        extraOptions: chartConfigs.greenChartOptions,
+        chartData: {
+            labels: [],
+            datasets: [{
+                label: "Revenue",
+                fill: true,
+                borderColor: config.colors.primary,
+                borderWidth: 2,
+                borderDash: [],
+                borderDashOffset: 0.0,
+                pointBackgroundColor: config.colors.primary,
+                pointBorderColor: 'rgba(255,255,255,0)',
+                pointHoverBackgroundColor: config.colors.primary,
+                pointBorderWidth: 20,
+                pointHoverRadius: 4,
+                pointHoverBorderWidth: 15,
+                pointRadius: 4,
+                data: [],
+            }]
+        },
+        gradientColors: config.colors.primaryGradient,
+        gradientStops: [1, 0.2, 0],
+    },
+    blueBarChart: {
+        extraOptions: chartConfigs.barChartOptions,
+        chartData: {
+            labels: [],
+            datasets: [{
+                label: "Audience",
+                fill: true,
+                borderColor: config.colors.info,
+                borderWidth: 2,
+                borderDash: [],
+                borderDashOffset: 0.0,
+                data: [],
+            }]
+        },
+        gradientColors: config.colors.primaryGradient,
+        gradientStops: [1, 0.4, 0],
+    },
     purpleLineChart: {
         extraOptions: chartConfigs.purpleChartOptions,
         chartData: {
             labels: [],
             datasets: [{
-                label: "My First dataset",
+                label: "Expenditure",
                 fill: true,
                 borderColor: config.colors.danger,
                 borderWidth: 2,
@@ -50,20 +116,84 @@ const getters = {
     myReservations(state){
         return state.reservations;
     },
+    bigLineChartData(state){
+        return state.bigLineChart.chartData;
+    },
+    bigLineChartColors(state){
+        return state.bigLineChart.gradientColors;
+    },
+    bigLineChartStops(state){
+        return state.bigLineChart.gradientStops;
+    },
+    bigLineChartOptions(state){
+        return state.bigLineChart.extraOptions;
+    },
+    greenLineChartData(state){
+        return state.greenLineChart.chartData;
+    },
+    greenLineChartColors(state){
+        return state.greenLineChart.gradientColors;
+    },
+    greenLineChartStops(state){
+        return state.greenLineChart.gradientStops;
+    },
+    greenLineChartOptions(state){
+        return state.greenLineChart.extraOptions;
+    },
+    blueBarChartData(state){
+        return state.blueBarChart.chartData;
+    },
+    blueBarChartStops(state){
+        return state.blueBarChart.gradientStops;
+    },
+    blueBarChartOptions(state){
+        return state.blueBarChart.extraOptions;
+    },
     purpleLineChartData(state){
-        console.log(state.purpleLineChart.chartData);
         return state.purpleLineChart.chartData;
     },
-    purpleLineChartGradient(state){
+    purpleLineChartStops(state){
         return state.purpleLineChart.gradientStops;
     },
-    purpleLineChartExtra(state){
+    purpleLineChartOptions(state){
         return state.purpleLineChart.extraOptions;
     },
     isMyPageLoading(state){
         return state.isLoading;
     }
 };
+
+function setStatistics(list, index){
+    state.expStatistics = {};
+
+    list.forEach(data => {
+        let target = JSON.stringify(data);
+        let count, date, price;
+        JSON.parse(target, function(key, value){
+            if(key === 'purchaseCount'){
+                count = Number(value);
+            }else if(key === 'price'){
+                price = Number(value) * count;
+            }else if(key === 'createdDate') {
+                date = value;
+            }
+        });
+
+        if(index != 2){
+            if(Object.keys(state.expStatistics).includes(date)){
+                state.expStatistics[date] = state.expStatistics[date] + price;
+            }else{
+                state.expStatistics[date] = price;
+            }
+        }else{
+            if(Object.keys(state.expStatistics).includes(date)){
+                state.expStatistics[date] = state.expStatistics[date] + count;
+            }else{
+                state.expStatistics[date] = count;
+            }
+        }
+    });
+}
 
 export const actions = {
     [MY_CONCERTS](context){
@@ -84,30 +214,27 @@ export const mutations = {
     [SET_MY_CONCERTS](state, concert){
         state.concerts = concert[0];
         state.reservations = concert[1];
-        //state.expStatistics = {};
+        state.totalRevenue = concert[2];
+        state.concertRevenue = concert[3];
 
-        state.reservations.forEach(reservation => {
-            let target = JSON.stringify(reservation);
-            let count, date, price;
-            JSON.parse(target, function(key, value){
-                if(key === 'purchaseCount'){
-                    count = Number(value);
-                }else if(key === 'price'){
-                    price = Number(value) * count;
-                }else if(key === 'createdDate') {
-                    date = value;
-                }
-            });
+        setStatistics(state.totalRevenue, 0);
 
-            if(Object.keys(state.expStatistics).includes(date)){
-                state.expStatistics[date] = state.expStatistics[date] + price;
-            }else{
-                state.expStatistics[date] = price;
-            }
-        });
+        state.bigLineChart.chartData.labels = Object.keys(state.expStatistics);
+        state.bigLineChart.chartData.datasets[0].data = Object.values(state.expStatistics);
+
+        setStatistics(state.concertRevenue, 1);
+
+        state.greenLineChart.chartData.labels = Object.keys(state.expStatistics);
+        state.greenLineChart.chartData.datasets[0].data = Object.values(state.expStatistics);
+
+        setStatistics(state.concertRevenue, 2);
+
+        state.blueBarChart.chartData.labels = Object.keys(state.expStatistics);
+        state.blueBarChart.chartData.datasets[0].data = Object.values(state.expStatistics);
+
+        setStatistics(state.reservations, 3);
 
         state.purpleLineChart.chartData.labels = Object.keys(state.expStatistics);
-        console.log(state.purpleLineChart.chartData.labels);
         state.purpleLineChart.chartData.datasets[0].data = Object.values(state.expStatistics);
 
         state.isLoading = false;
