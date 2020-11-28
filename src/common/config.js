8ebb3b38430fe8724ebb7a4dbece32f24e064efb
myungsegyo <myungsekyo@gmail.com>
2020-11-28 13:45:43 +0900

    [build] new build

--- src/common/config.js
+++ src/common/config.js
@@ -1,4 +1,5 @@
 // export const API_URL = "http://localhost:8080/api";
+//export const API_URL = "https://localhost/api";
 export const API_URL = "https://humming-inside.kro.kr/api";
 export default API_URL;
 
